1c9b1aff5df0bd679081555749bc5e8e71aaafd0
profgrammer <javarep4199@gmail.com>
2019-09-10 17:38:02 +0530

    Updates

---
+++ precache-manifest.6bd144509ad0a73beaa8f906208e289c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "713a88ee0835cfcf16881592f77722a2",
+    "url": "/TerasologyTutorialJourney/index.html"
+  },
+  {
+    "revision": "02ee4aa94d5c3b6d2ec6",
+    "url": "/TerasologyTutorialJourney/static/css/main.9d623f32.chunk.css"
+  },
+  {
+    "revision": "fd385773f44a875756b2",
+    "url": "/TerasologyTutorialJourney/static/js/2.31bf8e74.chunk.js"
+  },
+  {
+    "revision": "02ee4aa94d5c3b6d2ec6",
+    "url": "/TerasologyTutorialJourney/static/js/main.334bdc81.chunk.js"
+  },
+  {
+    "revision": "61cd349886793326e903",
+    "url": "/TerasologyTutorialJourney/static/js/runtime~main.584550e5.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/TerasologyTutorialJourney/precache-manifest.6bd144509ad0a73beaa8f906208e289c.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/TerasologyTutorialJourney/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
